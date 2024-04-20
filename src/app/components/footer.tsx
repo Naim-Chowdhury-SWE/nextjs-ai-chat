78d52895c7dc87ca81b7195c478c7b2ade7f8232
Naim-Chowdhury-SWE <naim.91@hotmail.com>
2024-04-20 21:55:47 +0200

    [Fix] Deleted output as export

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -2,10 +2,12 @@ const Footer = () => {
   return (
     <footer className="text-center bg-gray-500 border-t -mt-32">
       <h3 className="text-2xl font-bold pt-4">Note!</h3>
-      <p className="font-semibold">
+      <p className="font-semibold pb-4">
         If no answer is being presented it means that my credits at OpenAI has
         run out
       </p>
+
+      <p>&copy; {new Date().getFullYear()} Naim Chowdhury</p>
     </footer>
   );
 };
